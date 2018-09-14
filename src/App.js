a965b09b107b8ee85b573821b98961435ab574e8
Aleksey Vozhdaev <soulburglar@gmail.com>
2018-09-14 12:10:05 +0300

    add deps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 // const App = props => <h1>Hello</h1>
 
 class App extends React.Component {
-  handleClicked = evt => console.log('Clicked')
+  // handleClicked = evt => console.log('Clicked');
   render() {
     return (
       <div>
-        <h1 className="test">Click red button</h1>
-        <div className="alert alert-primary" role="alert">
-          A simple primary alert—check it out!
-        </div>
-        <button onClick={this.handleClicked}>Click</button>
+        {/*<h1 className="test">Click red button</h1>*/}
+        {/*<div className="alert alert-primary" role="alert">*/}
+          {/*A simple primary alert—check it out!*/}
+        {/*</div>*/}
+        {/*<button onClick={this.handleClicked}>Click</button>*/}
       </div>
     )
   }
 }
 
-export default App
+export default App;
